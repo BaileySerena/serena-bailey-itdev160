94e5c5b502b0ee787ea91c4e27232266789848fc
serena Bailey <baileysn@gmatc.matc.edu>
2019-03-07 17:55:19 -0600

    First Message

---
+++ Activity-4/js/scripts.js
@@ -0,0 +1,14 @@
+var username;
+var greeting = 'Welcome ';
+username = 'Serena';
+var salutation = greeting + username + '!';
+
+greeting = document.getElementById(‘greeting’);
+el.textContent = greeting;
+
+var price = 10;
+var studentDiscount = .10;
+var studentPrice = price - (price * studentDiscount);
+
+price = document.getElementById('price');
+studentPrice = document.getElementById('Student-price');
